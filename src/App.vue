284c377d0fcd73a9712b524f03a6d0063fc61d35
mouuii <49775493+mouuii@users.noreply.github.com>
2023-08-31 15:16:21 +0800

    footer

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
 import Header from "@/components/common/Header.vue"
 import NavSwiper from '@/components/home/NavSwiper.vue'
 import NewGoodCourse from '@/components/home/NewGoodCourse.vue'
+import Foot from '@/components/common/Foot.vue'
 </script>
 
 <template>
     <Header/>
     <NavSwiper/>
     <NewGoodCourse/>
+    <Foot/>
 </template>
