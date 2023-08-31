e8fb1ff2baf5da24828049e262deb4d7c79f9d5c
mouuii <49775493+mouuii@users.noreply.github.com>
2023-08-31 10:50:57 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import Header from "@/components/common/Header.vue"
+import NavSwiper from '@/components/home/NavSwiper.vue'
+</script>
+
+<template>
+    <Header/>
+    <NavSwiper/>
+</template>
