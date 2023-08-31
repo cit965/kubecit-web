2f0ce700499cb5c8be426a03b63d815c5a6f4493
mouuii <49775493+mouuii@users.noreply.github.com>
2023-08-31 17:59:34 +0800

    添加登录页面路由

--- src/router/index.js
+++ src/router/index.js
@@ -12,10 +12,12 @@ const router = createRouter({
     {
       path: '/course',
       name: 'course',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('@/views/CourseView.vue')
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('@/views/LoginView.vue')
     }
   ]
 })
---
+++ src/views/LoginView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import Header from "@/components/common/Header.vue"
+</script>
+
+<template>
+ <Header></Header>
+
+ 登录页面
+</template>
