d873b3eaaa96e33cc9a3741f4877713fc9bde261
mouuii <49775493+mouuii@users.noreply.github.com>
2023-08-31 17:30:54 +0800

    课程页面

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,14 @@
+<script setup>
+import Header from "@/components/common/Header.vue"
+import NavSwiper from '@/components/home/NavSwiper.vue'
+import NewCourse from '@/components/home/NewGoodCourse.vue'
+import Footer from '@/components/common/Foot.vue'
+</script>
+
+
 <template>
-    
+	<Header></Header>
+	<NavSwiper></NavSwiper>
+	<NewCourse ></NewCourse>
+	<Footer></Footer>
 </template>
