6da494d484ddd1df585b98fb33a1687405605d7a
a92560 <925603297@qq.com>
2023-09-24 11:55:48 +0800

    feat: 重构 CourseItem 组件 + 新增讲师详情页面

--- src/router/index.js
+++ src/router/index.js
@@ -1,95 +1,85 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '@/views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "@/views/HomeView.vue";
 
 const router = createRouter({
-  history: createWebHistory(
-    import.meta.env.BASE_URL),
-  routes: [{
-      path: '/',
-      name: 'home',
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "home",
       component: HomeView,
     },
     {
-      path: '/course',
-      name: 'course',
-      component: () =>
-        import ('@/views/CourseView.vue'),
+      path: "/course",
+      name: "course",
+      component: () => import("@/views/CourseView.vue"),
     },
     {
-      path: '/login',
-      name: 'login',
-      component: () =>
-        import ('@/views/LoginView.vue'),
+      path: "/login",
+      name: "login",
+      component: () => import("@/views/LoginView.vue"),
     },
     {
-      path: '/register',
-      name: 'register',
-      component: () =>
-        import ('@/views/RegisterView.vue'),
+      path: "/register",
+      name: "register",
+      component: () => import("@/views/RegisterView.vue"),
     },
     {
-      path: '/course/detail',
-      name: 'courseDetail',
-      component: () =>
-        import ('@/views/CourseDetail.vue'),
+      path: "/course/detail",
+      name: "courseDetail",
+      component: () => import("@/views/CourseDetail.vue"),
     },
     {
-      path: '/challenge',
-      name: 'challenge',
-      component: () =>
-        import ('@/views/Challenge.vue'),
+      path: "/challenge",
+      name: "challenge",
+      component: () => import("@/views/Challenge.vue"),
     },
     {
-      path: '/order',
-      name: 'order',
-      component: () =>
-        import ('@/views/Order.vue'),
+      path: "/order",
+      name: "order",
+      component: () => import("@/views/Order.vue"),
     },
     {
-      path: '/communicate',
-      name: 'communicate',
-      component: () =>
-        import ('@/views/Communicate.vue'),
+      path: "/communicate",
+      name: "communicate",
+      component: () => import("@/views/Communicate.vue"),
     },
     {
-      path: '/policyUserService',
-      name: 'policyUserService',
-      component: () =>
-        import ('@/views/PolicyUserService.vue'),
+      path: "/policyUserService",
+      name: "policyUserService",
+      component: () => import("@/views/PolicyUserService.vue"),
     },
     {
-      path: '/policyUserPrivate',
-      name: 'policyUserPrivate',
-      component: () =>
-        import ('@/views/PolicyUserPrivate.vue'),
+      path: "/policyUserPrivate",
+      name: "policyUserPrivate",
+      component: () => import("@/views/PolicyUserPrivate.vue"),
     },
     {
-      path: '/vip',
-      name: 'vip',
-      component: () =>
-        import ('@/views/VipView.vue'),
+      path: "/vip",
+      name: "vip",
+      component: () => import("@/views/VipView.vue"),
     },
     {
-      path: '/about',
-      name: 'about',
-      component: () =>
-        import ('@/views/AboutView.vue'),
+      path: "/about",
+      name: "about",
+      component: () => import("@/views/AboutView.vue"),
     },
     {
-
-      path: '/PersonCenter',
-      name: 'PersonCenter',
-      component: () => import('@/views/PersonCenter.vue'),
+      path: "/PersonCenter",
+      name: "PersonCenter",
+      component: () => import("@/views/PersonCenter.vue"),
     },
-           {
-      path: '/contactUs',
-      name: 'contactUs',
-      component: () => import('@/views/ContactUs.vue')
-    }
-
+    {
+      path: "/contactUs",
+      name: "contactUs",
+      component: () => import("@/views/ContactUs.vue"),
+    },
+    {
+      path: "/teacher/detail",
+      name: "teacherDetail",
+      component: () => import("@/views/TeacherDetail.vue"),
+    },
   ],
-})
-
+});
 
 export default router;
-
